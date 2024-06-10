9bd79943b9a275eb80d288f19e48a9a03ad96221
unknown <jesbin joseph>
2024-06-10 19:35:57 +0300

    added sum of two numbers

---
+++ script.js
@@ -0,0 +1,37 @@
+// // Create a program that asks the user for two numbers and prints their sum
+// let num1 = 23
+// let num2 = 10
+// let result=(num1+num2)
+// console.log(result);
+
+// // Implement a program that calculates the area of a rectangle using given length and width
+// let length =10
+// let width =5
+// area=length*width
+// console.log(area);
+
+// Create a program that asks the user for their age and determines if they are eligible to vote (considering a voting age of 18).
+// let age =17
+// if (age<18){
+//     console.log("not eligible");
+// }
+// else {
+//     console.log('eligible to vote');
+// }
+
+// Write a script that takes a user's grade as input and outputs whether they passed or failed (assuming the passing grade is 60 or above).
+// let grade=70
+// let x=grade>=60 ? 'pass':'fail'
+// console.log(x);
+
+// Implement a program that checks if a user's entered number is positive, negative, or zero.
+// Get the user's input
+let userInput =-1
+
+if (userInput > 0) {
+    console.log("The number is positive.");
+} else if (userInput < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
